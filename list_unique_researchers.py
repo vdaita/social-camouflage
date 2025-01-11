c2de0631882d652458a0c52efaa07d3cca04679f
V Daita <vdaita@gmail.com>
2025-01-10 18:29:07 -0600

    updates to analysis

--- list_unique_researchers.py
+++ list_unique_researchers.py
@@ -3,10 +3,15 @@ import pandas as pd
 df = pd.read_csv("data/0721-month-analysis-data.csv")
 print(df.columns)
 
+df = df[df['Pre-2014 School'].isin(['CityU', 'CUHK', 'HKU', 'HKUST'])]
+pre_2014_school_researchers = df['Researcher Cleaned Name'].unique().tolist()
+
 df = df[df['Researcher Classification'] == 'Hong Kong']
-researchers = df['Researcher Cleaned Name'].unique().tolist()
+researcher_classification_researchers = df['Researcher Cleaned Name'].unique().tolist()
+
+researchers = list(set(pre_2014_school_researchers) - set(researcher_classification_researchers))
 
 new_df = pd.DataFrame(researchers, columns=['Name'])
-new_df.to_csv("homepage_data_2.csv", index=False)
+new_df.to_csv("homepage_data_3.csv", index=False)
 
 print(researchers, "Length: ", len(researchers))
